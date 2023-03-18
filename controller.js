862a07171af50ef5a903f9542275b4f8af5afc8e
rio <riovectored@gmail.com>
2023-03-18 22:18:46 +0700

    menambahkan code menampilkan data

--- controller.js
+++ controller.js
@@ -6,3 +6,14 @@ var connection = require('./koneksi');
 exports.index = function(req,res){
 response.ok("APlikasi Rest APi ini berjalan.",res);
 };
+
+//menampilkan data dr database
+exports.tampilsemuamahasiswa = function name(req,res) {
+connection.query('select * from mahasiswa', function(error, row, fields){
+    if(error){
+        connection.log(error);
+    } else{
+        response.ok(row, res)
+    }
+}); 
+};
