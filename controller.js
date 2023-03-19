91e1ef49f5a4159fd2e363c67bc415b720efbde5
rio <riovectored@gmail.com>
2023-03-19 16:32:08 +0700

    menambahkan kode menampilkan berdasarkan id

--- controller.js
+++ controller.js
@@ -8,12 +8,25 @@ response.ok("APlikasi Rest APi ini berjalan.",res);
 };
 
 //menampilkan data dr database
-exports.tampilsemuamahasiswa = function name(req,res) {
+exports.tampilsemuamahasiswa = function (req,res) {
 connection.query('select * from mahasiswa', function(error, row, fields){
     if(error){
-        connection.log(error);
+        console.log(error);
     } else{
         response.ok(row, res)
     }
 }); 
+};
+
+//menampilkan semua data mahasiswa berdasarkan id
+exports.tampilberdasarkanid = function(req,res){
+let id = req.params.id;
+connection.query('select * from mahasiswa where id_mahasiswa = ?', [id], 
+function(error, rows, fields){
+    if(error){
+        console.log(error);
+    }else {
+        response.ok(rows, res);
+    }
+});
 };
