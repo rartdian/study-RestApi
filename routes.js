a173483ac96a6e044efc0032f738df096c6b3a7f
rio <riovectored@gmail.com>
2023-03-19 16:10:20 +0700

    menambahkan route tampilberdasarkanid

--- routes.js
+++ routes.js
@@ -10,4 +10,6 @@ module.exports = function(app){
 //untuk menampilkan data dari controller
 app.route('/tampil')
     .get(jsonku.tampilsemuamahasiswa);
+app.route('/tampil/:id')
+    .get(jsonku.tampilberdasarkanid)
 }
